6e5ff0aa70f9331f89023711feed3144750a7eaf
Alexis Chesneau <[alexischesneau64@gmail.com](mailto:alexischesneau64@gmail.com)>
2023-07-25 13:58:37 +0200

    ajout des icons

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,12 +1,31 @@
+import { NavLink } from "react-router-dom";
+import { BiSolidUser, BiSolidCategoryAlt } from "react-icons/bi";
+import { TbMapPinSearch } from "react-icons/tb";
+import { HiChatBubbleOvalLeftEllipsis } from "react-icons/hi2";
+import { RiFolderUserFill } from "react-icons/ri";
+
 function NavBar() {
   return (
-    <header>
+    <header className="bottom-0 fixed w-full py-6">
       <nav>
-        <ul>
-          <li className="text-[100px]">icon</li>
-          <li>icon</li>
-          <li>icon</li>
-          <li>icon</li>
+        <ul className="flex justify-evenly">
+          <NavLink to="/">
+            <li>
+              <TbMapPinSearch />
+            </li>
+          </NavLink>
+          <li>
+            <BiSolidCategoryAlt />
+          </li>
+          <li>
+            <RiFolderUserFill />
+          </li>
+          <li>
+            <HiChatBubbleOvalLeftEllipsis />
+          </li>
+          <li>
+            <BiSolidUser />
+          </li>
         </ul>
       </nav>
     </header>
