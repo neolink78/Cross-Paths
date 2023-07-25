e615619e356e25e809eff514ec77f317806ca033
Alexis Chesneau <[alexischesneau64@gmail.com](mailto:alexischesneau64@gmail.com)>
2023-07-25 11:15:31 +0200

    setup tailwind

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+function NavBar() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li className="text-[100px]">icon</li>
+          <li>icon</li>
+          <li>icon</li>
+          <li>icon</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default NavBar;
