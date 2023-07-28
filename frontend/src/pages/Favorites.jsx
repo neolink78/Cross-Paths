c598296760de72adee46b93e307461cb60aafb48
Alexis Chesneau <[alexischesneau64@gmail.com](mailto:alexischesneau64@gmail.com)>
2023-07-28 14:15:54 +0200

    create user_page

--- frontend/src/pages/Favorites.jsx
+++ frontend/src/pages/Favorites.jsx
@@ -2,53 +2,58 @@ function Favorites() {
   const favorites = [
     {
       id: 1,
-      img: "",
-      name: "Personne 1",
+      img: "./assets/images/pictures/picture_01.png",
+      name: "Jade",
     },
     {
       id: 2,
-      img: "",
-      name: "Personne 2",
+      img: "./assets/images/pictures/picture_02.png",
+      name: "Louise",
     },
     {
       id: 3,
-      img: "",
-      name: "Personne 3",
+      img: "./assets/images/pictures/picture_03.png",
+      name: "Emma",
     },
     {
       id: 4,
-      img: "",
-      name: "Personne 4",
+      img: "./assets/images/pictures/picture_04.png",
+      name: "Alice",
     },
     {
       id: 5,
-      img: "",
-      name: "Personne 5",
+      img: "./assets/images/pictures/picture_05.png",
+      name: "Ambre",
     },
     {
       id: 6,
-      img: "",
-      name: "Personne 6",
+      img: "./assets/images/pictures/picture_06.png",
+      name: "Lina",
     },
     {
       id: 7,
-      img: "",
-      name: "Personne 7",
+      img: "./assets/images/pictures/picture_07.png",
+      name: "Rose",
     },
     {
       id: 8,
-      img: "",
-      name: "Personne 8",
+      img: "./assets/images/pictures/picture_08.png",
+      name: "Chloé",
     },
     {
       id: 9,
-      img: "",
-      name: "Personne 9",
+      img: "./assets/images/pictures/picture_09.png",
+      name: "Mia",
     },
     {
       id: 10,
-      img: "",
-      name: "Personne 10",
+      img: "./assets/images/pictures/picture_10.png",
+      name: "Léa",
+    },
+    {
+      id: 11,
+      img: "./assets/images/pictures/picture_11.png",
+      name: "Anna",
     },
   ];
 
@@ -57,12 +62,14 @@ function Favorites() {
       {favorites.map((favorite) => (
         <div
           key={favorite.id}
-          className="h-52 w-36 bg-slate-400 m-3 rounded-md"
+          className="h-52 w-36 m-3 flex flex-col justify-center"
         >
-          <img src={favorite.img} alt={favorite.img} />
-          <h1 className="flex h-full items-end p-4 font-bold">
-            {favorite.name}
-          </h1>
+          <img
+            className="h-full rounded-md object-cover"
+            src={favorite.img}
+            alt={favorite.img}
+          />
+          <h1 className="font-bold">{favorite.name}</h1>
         </div>
       ))}
     </section>
