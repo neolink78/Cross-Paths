f4500f98ec3f94f0519319b6b812a3bae7b1b322
Thibaut Resse <thibaut.resse@outlook.fr>
2023-07-28 18:17:14 +0200

    logic made to change categories

--- frontend/src/pages/Search.jsx
+++ frontend/src/pages/Search.jsx
@@ -4,55 +4,62 @@ import { PiArrowCounterClockwiseBold } from "react-icons/pi";
 import { useEffect, useState } from "react";
 import * as Services from "../services/People.service";
 
-function Search() {
+export default function Search({ selectedCategory }) {
   const [nextPage, setNextPage] = useState(0);
   const [people, setPeople] = useState(null);
 
   useEffect(() => {
-    Services.getPeople().then((res) => {
+    selectedCategory === "à la recherche de l'amour" && Services.getPeople().then((res) => {
       setPeople(res);
     });
   }, []);
 
   return (
-    <section className="h-[80vh] mt-[10vh] flex justify-center items-center ">
-      {people !== null && people.length > nextPage ? (
-        <div id="search" className="h-[400px] w-[300px] bg-gray-300 rounded-md">
-          {/* cette div représente une personne */}
-          <h1 className="p-6 text-xl">{people[nextPage].name}</h1>
-          <p>{people[nextPage].description}</p>
-          <div className="flex justify-evenly h-[297px] items-end py-8">
-            <button
-              id="cross"
-              type="button"
-              onClick={() => setNextPage(nextPage + 1)}
-            >
-              <ImCross />
-            </button>
-          <button onClick={() => setNextPage(nextPage - 1)} id="rewind" type="button">
-            <PiArrowCounterClockwiseBold />
-          </button>
-            <button
-              id="star"
-              type="button"
-              onClick={() => setNextPage(nextPage + 1)}
-            >
-              <BsStarFill />
-            </button>
-            <button
-              id="heart"
-              type="button"
-              onClick={() => setNextPage(nextPage + 1)}
-            >
-              <BsHeartFill />
-            </button>
+    <>
+        <h1>à redisposer {selectedCategory}</h1>
+      <section className="h-[80vh] mt-[10vh] flex justify-center items-center ">
+        {people !== null && people.length > nextPage ? (
+          <div
+            id="search"
+            className="h-[400px] w-[300px] bg-gray-300 rounded-md"
+          >
+            <h1 className="p-6 text-xl">{people[nextPage].name}</h1>
+            <p>{people[nextPage].description}</p>
+            <div className="flex justify-evenly h-[297px] items-end py-8">
+              <button
+                id="cross"
+                type="button"
+                onClick={() => setNextPage(nextPage + 1)}
+              >
+                <ImCross />
+              </button>
+              <button
+                onClick={() => setNextPage(nextPage - 1)}
+                id="rewind"
+                type="button"
+              >
+                <PiArrowCounterClockwiseBold />
+              </button>
+              <button
+                id="star"
+                type="button"
+                onClick={() => setNextPage(nextPage + 1)}
+              >
+                <BsStarFill />
+              </button>
+              <button
+                id="heart"
+                type="button"
+                onClick={() => setNextPage(nextPage + 1)}
+              >
+                <BsHeartFill />
+              </button>
+            </div>
           </div>
-        </div>
-      ) : (
-        "ya plus rien !"
-      )}
-    </section>
+        ) : (
+          "ya plus rien !"
+        )}
+      </section>
+    </>
   );
 }
-
-export default Search;
